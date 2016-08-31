7fbe5d2b6cda99a7ea5b1b193d91782ef9fa7934
Maxf <maximefouques@gmail.com>
2016-08-31 12:08:21 +0200

    Update main.js

--- public/js/sluck.js
+++ public/js/sluck.js
@@ -25,11 +25,33 @@ module.exports = function(){
     var currentUser = user.getUser();
 
     // on form submit , send event with message
-    $('.formMessage').on('submit',function(){
+    $('.submitMessage').on('click',function(){
         var message = $('#message').val();
         currentUser.socket.emit('registerMessage', {chanID: 1, message: message});
     });
+
+    currentUser.socket.on('newMessage', function(data){
+        console.log(data);
+        showMessage(data);
+    })
 };
+
+
+   function showMessage(data){
+
+       var containerMessage = $('.messageContainer');
+       var article = document.createElement('article').addClass('containerMessage');
+       var pseudo = document.createElement('p').addClass('pseudo');
+       var date = document.createElement('span').addClass('messageDate');
+       var message = document.createElement('p').addClass('messageContent');
+       pseudo.innerHTML = data[0].id;
+       message.innerHTML = data[0].message;
+       date.innerHTML = data[0].date;
+       pseudo.appendChild(date);
+       article.appendChild(pseudo);
+       article.appendChild(message);
+       containerMessage.appendChild(article);
+    }
 
 },{"./user.js":4}],3:[function(require,module,exports){
 /**

848fe7c6fba629470801a2988204362c09ea63e7
Maxf <maximefouques@gmail.com>
2016-08-31 12:18:20 +0200

    Temp jquery message

--- public/js/modules/message.js
+++ public/js/modules/message.js
@@ -18,17 +18,6 @@ module.exports = function(){
 
 
    function showMessage(data){
-
        var containerMessage = $('.messageContainer');
-       var article = document.createElement('article').addClass('containerMessage');
-       var pseudo = document.createElement('p').addClass('pseudo');
-       var date = document.createElement('span').addClass('messageDate');
-       var message = document.createElement('p').addClass('messageContent');
-       pseudo.innerHTML = data[0].id;
-       message.innerHTML = data[0].message;
-       date.innerHTML = data[0].date;
-       pseudo.appendChild(date);
-       article.appendChild(pseudo);
-       article.appendChild(message);
-       containerMessage.appendChild(article);
+       containerMessage.append("<div>"+data[0].message+"</div>");
     }

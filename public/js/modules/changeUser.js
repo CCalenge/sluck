6c948bf1c68a0534bf81af4f9c249e6495b716d5
maxoon50 <desfouxmaxime@gmail.com>
2016-09-06 10:56:43 +0200

    Fix currentUser on change user

--- public/js/modules/changeUser.js
+++ public/js/modules/changeUser.js
@@ -1,26 +1,31 @@
 module.exports = function() {
+
     var User = require('./user');
 
     // click event on close window and currentUserName (top left page)
     $('.closeModal').on('click', function() {
-        $('#containerChangeUser').addClass("hide");
+
+        $('#containerChangeUser').fadeOut();
     });
 
     $('#currentUser').on('click', function() {
         // on click , show the div containerChangeUser
-        $('#containerChangeUser').removeClass("hide");
+        $('#containerChangeUser').fadeIn();
 
         //get the user, then emit the request to have the bdd data
+
         var currentUser = User.getUser();
-        currentUser.socket.emit('askUserInfo', currentUser.pseudo);
+
+        currentUser.socket.emit('askUserInfo',$('#currentUser').html());
 
         // with the response, we can set some value for the input
         currentUser.socket.on('returnUserData', function(result) {
 
             bddResult = result[0];
             check = true;
+
             // put the default value for the pseudo
-            $('.pseudoInput').val(bddResult.pseudo);
+            $('.pseudoInput').val($('#currentUser').html());
             // check the password to define a new one
             $('.passwd').on('blur', function() {
 
@@ -68,15 +73,15 @@ module.exports = function() {
                     $(this).text('!').css('fontSize', 40);
 
                 } else {
-                    // refresh input
-                    $('#containerChangeUser').find('input[type=password]').val("");
 
-                    $('#containerChangeUser').addClass("hide");
+
+                    $('#containerChangeUser').fadeOut();
                     check = true;
+
                     userData.lastPseudo = bddResult.pseudo;
                     userData.pseudo = $('.pseudoInput').val();
 
-                    // if there is a new password userdata password is input val, otherwise we take the bdd.result
+                    // if there is a new password, userdata password is input val, otherwise we take the bdd.result
                     if ($('.checkNewPasswd').val().length > 0) {
                         userData.password = $('.checkNewPasswd').val();
                     } else {
@@ -87,7 +92,10 @@ module.exports = function() {
                     currentUser.socket.emit('updateUser', userData);
                     //change the currentuserName on the user client side
                     $('#currentUser').text(userData.pseudo);
+                    // error message
                     $(this).css('fontSize', 10).text('modifier');
+                    // refresh
+                    $('#containerChangeUser').find('input[type=password]').val("");
                 };
             });
         });
